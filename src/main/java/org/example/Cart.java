f90d6ac7338d2e0511d8d1cab2078761738e43ea
Hubi_Core <hbstojek@wp.pl>
2026-02-01 19:26:48 +0100

    fix: fix

--- src/main/java/org/example/Cart.java
+++ src/main/java/org/example/Cart.java
@@ -28,28 +28,17 @@ public class Cart {
     }
 
     public static Cart generate_random_cart() {
-        String[] colors = {"RED", "GREEN", "BLUE", "YELLOW", "WILD"};
+        String[] colors = {"RED", "GREEN", "BLUE", "YELLOW"};
         String[] weights = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-                "+2", "⏸", "↺", "W", "W4"};
+                "+2", "⏸", "↺"};
 
         int colorIndex = (int)(Math.random() * colors.length);
         int weightIndex = (int)(Math.random() * weights.length);
-
-        // Jeśli karta jest WILD, to wartość musi być odpowiednia
-        if (colors[colorIndex].equals("WILD")) {
-            weightIndex = (int)(Math.random() * 2) + 13; // WILD lub WILD_DRAW4
-        }
 
         return new Cart(colors[colorIndex], weights[weightIndex]);
     }
 
     public boolean canPlayOn(Cart otherCard) {
-
-        if (this.kolor.equals("WILD")) {
-            return true;
-        }
-
-        // Można zagrać jeśli ten sam kolor lub ta sama wartość
         return this.kolor.equals(otherCard.kolor) ||
                 this.wartosc.equals(otherCard.wartosc);
     }
